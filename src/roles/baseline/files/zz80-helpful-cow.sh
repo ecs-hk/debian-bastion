f318d015125b195bb24ce2dfc16958d1f87c6149
ecs-hk <8094390+ecs-hk@users.noreply.github.com>
2023-09-01 15:34:15 -0400

    Make tests more cautious (for non bash accounts)

--- src/roles/baseline/files/zz80-helpful-cow.sh
+++ src/roles/baseline/files/zz80-helpful-cow.sh
@@ -1,13 +1,20 @@
 # Managed by Ansible
 
-# Only run if in interactive shell..
-if [[ $- == *i* ]]  ; then
+ps -p ${$} | grep -q 'bash'
+_rc1=${?}
+
+printf "${-}" | grep -q 'i'
+_rc2=${?}
+
+# Only run if bash and if in interactive shell
+if [ ${_rc1} -eq 0 ] && [ ${_rc2} -eq 0 ]  ; then
 
         which fortune > /dev/null 2>&1
         _rc1=${?}
         which cowsay > /dev/null 2>&1
         _rc2=${?}
 
+        # Only run if both programs are installed
         if [ ${_rc1} -eq 0 ] && [ ${_rc2} -eq 0 ] ; then
                 fortune -s | cowsay
         fi
